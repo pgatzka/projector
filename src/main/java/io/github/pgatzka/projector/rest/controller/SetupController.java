bf444027d98b791094594f035f8fa818a1d82e8c
Philipp Gatzka <phil.gatzka@gmail.com>
2026-05-06 20:55:47 +0200

    feat(rest): SetupController POST /api/setup

---
+++ src/main/java/io/github/pgatzka/projector/rest/controller/SetupController.java
@@ -0,0 +1,30 @@
+package io.github.pgatzka.projector.rest.controller;
+
+import io.github.pgatzka.projector.jooq.tables.pojos.Account;
+import io.github.pgatzka.projector.rest.dto.MeResponse;
+import io.github.pgatzka.projector.rest.dto.SetupRequest;
+import io.github.pgatzka.projector.rest.service.SetupService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/setup")
+public class SetupController {
+
+    private final SetupService setupService;
+
+    public SetupController(SetupService setupService) {
+        this.setupService = setupService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MeResponse> setup(@Valid @RequestBody SetupRequest request) {
+        Account created = setupService.completeSetup(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(MeResponse.of(created));
+    }
+}
