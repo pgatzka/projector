badb26c54a554b7c027c465abb8d7d1f5cd46eab
Philipp Gatzka <phil.gatzka@gmail.com>
2026-05-06 16:49:34 +0200

    test: Cucumber JUnit Platform Suite + Spring Boot test wiring

---
+++ src/test/java/io/github/pgatzka/projector/e2e/RunCucumberIT.java
@@ -0,0 +1,17 @@
+package io.github.pgatzka.projector.e2e;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, summary")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.github.pgatzka.projector.e2e")
+public class RunCucumberIT {
+}
